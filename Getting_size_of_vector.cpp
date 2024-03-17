b29867d66e3fbff6c82eb80dabd1f726360d6f81
MadhavaKandala <kpmadhava06@gmail.com>
2024-03-17 14:08:16 +0530

    add elements at index in vector

---
+++ Getting_size_of_vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Creating a vector and adding elements
+    vector<int> vec = {10, 20, 30};
+
+    // Getting the size of the vector
+    cout << "Size of the vector: " << vec.size() << endl;
+
+    return 0;
+}
